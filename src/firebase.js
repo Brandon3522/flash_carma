6f0a1961cfaf0b8fdc72142eca49e54f286beb7e
Brandon Sitz <Brandonsitz17@gmail.com>
2022-10-05 10:55:49 -0500

    Firebase initialization

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs8RdSmy7ZUGtXs19U9mkIN2htHEkcFJ0",
+  authDomain: "flash-carma.firebaseapp.com",
+  projectId: "flash-carma",
+  storageBucket: "flash-carma.appspot.com",
+  messagingSenderId: "523520490614",
+  appId: "1:523520490614:web:a927bf2dbe454126d03357"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
