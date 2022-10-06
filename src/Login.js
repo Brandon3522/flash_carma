906d099f891930f02ecd4912fe4369d3649ec51c
Opuli1 <adrianwest914@gmail.com>
2022-10-06 10:11:01 -0500

    modified Login

--- src/Login.js
+++ src/Login.js
@@ -10,21 +10,19 @@ import {
   Button,
   Heading,
   useColorModeValue,
-  ChakraProvider,
 } from '@chakra-ui/react';
 
 function Login() {
   return (
-    <ChakraProvider>
-       <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          </Stack>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+        </Stack>
         <Box
           rounded={'lg'}
           bg={useColorModeValue('white', 'gray.700')}
@@ -57,10 +55,8 @@ function Login() {
             </Stack>
           </Stack>
         </Box>
-        </Stack>
-        </Flex>
-    </ChakraProvider>
-   
+      </Stack>
+    </Flex>
   );
 }
 
