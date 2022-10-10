162dd6e2584377c8ef795b1bc66e0d970d4cd61c
Brandon Sitz <Brandonsitz17@gmail.com>
2022-10-10 11:29:49 -0500

    Navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from "@chakra-ui/react";
 import Login from './Login';
+import Navbar from './Navbar';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -9,7 +10,8 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ChakraProvider>
-      <Login />
+      {/* <Login /> */}
+      <Navbar />
     </ChakraProvider>
   </StrictMode>
 );
