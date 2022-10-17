12c700d07c3b66ebda61d76c11889f75b46fd74a
Opuli1 <adrianwest914@gmail.com>
2022-10-17 10:37:23 -0500

    moved login to page folder and adjusted imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from "@chakra-ui/react";
-import { SignUp } from './pages';
+import { SignUp, Login } from './pages';
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Login';
 import Results from './Results';
 
 const container = document.getElementById('root');
