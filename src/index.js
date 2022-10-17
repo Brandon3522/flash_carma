47fa511ff4880634cda9c5865df737d81e879bbf
Opuli1 <adrianwest914@gmail.com>
2022-10-17 10:44:27 -0500

    moved Results to pages and adjusted imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from "@chakra-ui/react";
-import { SignUp, Login, Home } from './pages';
+import { SignUp, Login, Home, Results } from './pages';
 import Navbar from './Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Results from './Results';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
