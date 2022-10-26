9024013c2b85162fe2ca44da25889fe72d6a69fb
Opuli1 <adrianwest914@gmail.com>
2022-10-26 10:41:50 -0500

    changing the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,39 @@
 import React, { useState } from 'react';
-import { Text, Flex, Link, Input, Button, Box, Image } from '@chakra-ui/react';
+import { Switch, Flex, Link, Input, Button, Box, Image } from '@chakra-ui/react';
 import logo from './images/logo.png'
 import { Link as ReachLink } from 'react-router-dom';
 
 function Navbar() {
-  const [scroll, setScroll] = useState(false);
-
-  const changeScroll = () =>
-    document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
-      ? setScroll(true)
-      : setScroll(false);
-
-  window.addEventListener('scroll', changeScroll);
-
+  const [display, changeDisplay] = useState('none')
   return (
-    <Box>
+    <Flex>
       <Flex
-        h="10vh"
-        alignItems="center"
-        p="6"
-        boxShadow={scroll ? 'base' : 'none'}
-        position="sticky"
-        top="0"
-        zIndex="sticky"
-        w="full"
-        backgroundColor="grey"
-        color="white"
-        fontSize="large"
-        justifyContent="space-between"
+        position='fixed'
+        top='1rem'
+        right='1rem'
+        align='center'
       >
-        <Link as={ReachLink} to="/home">
-          <Image
-            src={logo}
-            // objectFit="cover"
-            mt={'8'}
-            ml={'-10'}
-            // width="100%"
-            // height="auto"
-          />
-        </Link>
+        <Flex display={['none', 'none', 'flex', 'flex']}>
+          <Link as={ReachLink} to='\home'>
+            <Button
+              as='a'
+              variant='ghost'
+              aria-label='home'
+              my={5}
+              w='100%'>
+                Home
+            </Button>
+          </Link>
 
-        {/* Add links */}
-        <Link mr="10">View Study Decks</Link>
-        <Link>Study Session</Link>
-
-        <Flex>
-          <Input
-            placeholder="Search"
-            backgroundColor="white"
-            textColor="grey"
-            ml="10"
-            size="md"
-            htmlSize={40}
-            width="auto"
-          ></Input>
-          <Button fontSize="large" ml="3" color="white" variant="link">
-            Search
-          </Button>
-        </Flex>
-
-        {/* Add links */}
-        <Flex>
-          <Link mr="10">Settings</Link>
-          <Link>Profile</Link>
+      
+          //view
+          //session
+          //settings
+          //profile
         </Flex>
       </Flex>
-    </Box>
-  );
+    </Flex>
+  )
 }
 
 export default Navbar;
