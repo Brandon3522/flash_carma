a28baff6229ba96811d938cadedc6cf124e9a434
JMoore143 <112124847+JMoore143@users.noreply.github.com>
2022-11-03 13:59:09 -0700

    in View Study Decks page, commented out link overlays for testing

--- src/pages/ViewStudyDecksPage/ViewStudyDecks.js
+++ src/pages/ViewStudyDecksPage/ViewStudyDecks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, Flex, Link, Input, Button, Box, Heading, Spacer, Grid, GridItem, 
-         Linkbox, LinkOverlay} from '@chakra-ui/react';
+         Linkbox, LinkOverlay } from '@chakra-ui/react';
 
 export const ViewStudyDecks = () => {
 
@@ -13,28 +13,26 @@ export const ViewStudyDecks = () => {
        {/* Grid of Study Decks */}
        <Grid templateColumns='repeats(4, 1fr)' gap={14}>
          {/* Create new study deck button */}
-         <LinkOverlay href='#'>
-            <GridItem w='100px' h='100px' bg='chartreuse'>
-
-            </GridItem>
-         </LinkOverlay>
+         {/* <LinkOverlay href='#'> */}
+            <GridItem w='100px' h='100px' bg='chartreuse'>Sample text</GridItem>
+         {/* </LinkOverlay> */}
          
          {/* From here on, it's the user's existing study decks */}
-         <LinkOverlay href='#'>
+         {/* <LinkOverlay href='#'> */}
            <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 1</GridItem>
-         </LinkOverlay>
+         {/* </LinkOverlay> */}
 
-         <LinkOverlay href='#'>
+         {/* <LinkOverlay href='#'> */}
          <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 2</GridItem>
-         </LinkOverlay>
+         {/* </LinkOverlay> */}
 
-         <LinkOverlay href='#'>
+         {/* <LinkOverlay href='#'> */}
          <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 3</GridItem>
-         </LinkOverlay>
+         {/* </LinkOverlay> */}
 
-         <LinkOverlay href='#'>
+         {/* <LinkOverlay href='#'> */}
          <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 4</GridItem>
-         </LinkOverlay>
+         {/* </LinkOverlay> */}
        </Grid>
      </Flex>
    )
