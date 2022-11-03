44e963485e6cfb5c056eae91016c3c07c3d4ea88
jramosgarcia <joseph.ramosgarcia@gmail.com>
2022-11-03 13:37:20 -0500

    study deck selection routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SignUp, Login, Home, Edit, Study, Results, ViewStudyDecks, Settings } from './pages';
+import { SignUp, Login, Home, Edit, Study, Results, ViewStudyDecks, Settings, StudyDeckSelection} from './pages';
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -13,8 +13,9 @@ function App() {
           <Route path='/results' element={<><Navbar></Navbar><Results></Results></>} />
           <Route path='/study' element={<><Navbar></Navbar><Study></Study></>} />
           <Route path='/edit' element={<><Navbar></Navbar><Edit></Edit></>} />
-          <Route path='/view' element={<><Navbar></Navbar><ViewStudyDecks></ViewStudyDecks></>} />
+          <Route path='/viewstudydecks' element={<><Navbar></Navbar><ViewStudyDecks></ViewStudyDecks></>} />
           <Route path='/settings' element={<><Navbar></Navbar><Settings></Settings></>} />  
+          <Route path='/studydeckselection' element={<><Navbar></Navbar><StudyDeckSelection></StudyDeckSelection></>} /> 
         </Routes>
       </BrowserRouter>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,4 @@ export * from './EditPage';
 export * from './StudyPage';
 export * from './ViewStudyDecksPage'
 export * from './ProfileSettingsPage';
+export * from './StudyDeckSelectionPage'
