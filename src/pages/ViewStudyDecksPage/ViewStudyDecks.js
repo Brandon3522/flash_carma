babbf1c902185c7e1291b3db189d1cc45cf569ff
jramosgarcia <joseph.ramosgarcia@gmail.com>
2022-11-03 17:09:39 -0500

    fixes

--- src/pages/ViewStudyDecksPage/ViewStudyDecks.js
+++ src/pages/ViewStudyDecksPage/ViewStudyDecks.js
@@ -3,7 +3,7 @@ import { Text, Flex, Link, Input, Button, Box, Heading, Spacer, Grid, GridItem,
          LinkBox, LinkOverlay } from '@chakra-ui/react';
 
 export const ViewStudyDecks = () => {
-  
+
         /*JavaScript goes here */
      
         
@@ -51,45 +51,3 @@ export const ViewStudyDecks = () => {
          </>
        )
      }
-    
-   return (
-    <Flex>
-       <Heading as='h3' size='xl'>Study Decks</Heading>
-
-       {/* Grid of Study Decks */}
-       <Grid templateColumns='repeat(4, 1fr)' gap={10}>
-         {/* Create new study deck button */}
-         <LinkBox>
-           <LinkOverlay href='#'>
-             <GridItem w='100px' h='100px' bg='chartreuse'>Sample text</GridItem>
-           </LinkOverlay> 
-         </LinkBox>
-         
-         {/* From here on, it's the user's existing study decks */}
-         <LinkBox>
-           <LinkOverlay href='#'>
-             <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 1</GridItem>
-           </LinkOverlay> 
-         </LinkBox>
-
-         <LinkBox>
-           <LinkOverlay href='#'>
-             <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 2</GridItem>
-           </LinkOverlay> 
-         </LinkBox>
-
-         <LinkBox>
-           <LinkOverlay href='#'>
-             <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 3</GridItem>
-           </LinkOverlay> 
-         </LinkBox>
-
-         <LinkBox>
-           <LinkOverlay href='#'>
-             <GridItem w='100px' h='100px' bg='chartreuse'>Study Deck 4</GridItem>
-           </LinkOverlay> 
-         </LinkBox>
-       </Grid>
-     </Flex>
-   )
- }
